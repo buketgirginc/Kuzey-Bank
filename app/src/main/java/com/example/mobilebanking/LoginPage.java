143fbad243e21f5362325f016490ab77caf9d25e
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2024-05-10 18:05:30 +0300

    login-page

--- app/src/main/java/com/example/mobilebanking/LoginPage.java
+++ app/src/main/java/com/example/mobilebanking/LoginPage.java
@@ -1,17 +1,25 @@
 package com.example.mobilebanking;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginPage extends AppCompatActivity {
+import com.example.mobilebanking.databinding.ActivityLoginPageBinding;
 
+public class LoginPage extends AppCompatActivity {
+    ActivityLoginPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.goToSignUp.setOnClickListener( v -> {
+            Intent intent = new Intent(this, SignupPageActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
