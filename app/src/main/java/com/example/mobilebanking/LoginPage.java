975502608502a8fa22d0be771d5fd723b68cc8c5
buket <buketgirginc41@gmail.com>
2024-05-10 16:08:14 +0300

    init

---
+++ app/src/main/java/com/example/mobilebanking/LoginPage.java
@@ -0,0 +1,17 @@
+package com.example.mobilebanking;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mobilebanking/SignupPageActivity.java
@@ -0,0 +1,15 @@
+package com.example.mobilebanking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_page);
+
+    }
+}
