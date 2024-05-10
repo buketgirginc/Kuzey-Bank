77ab5b9ba36946d9ca5ad34cd68278a58c3b5991
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2024-05-10 17:31:43 +0300

    sign-up-page-made-work

--- app/src/main/java/com/example/mobilebanking/SignupPageActivity.java
+++ app/src/main/java/com/example/mobilebanking/SignupPageActivity.java
@@ -1,15 +1,43 @@
 package com.example.mobilebanking;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignupPageActivity extends AppCompatActivity {
+import com.example.mobilebanking.databinding.ActivitySignupPageBinding;
+import com.example.mobilebanking.helper.DatabaseHelper;
+import com.example.mobilebanking.model.Musteri;
 
+public class SignupPageActivity extends AppCompatActivity {
+    ActivitySignupPageBinding binding;
+    DatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_page);
+        databaseHelper = new DatabaseHelper(this);
+        binding = ActivitySignupPageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Tıklandı");
+                String fullname = binding.fullname.toString();
+                String tc = binding.username.toString();
+                String sifre = binding.password.toString();
+                System.out.println("fname = " + fullname);
+                System.out.println("tc = " + tc );
+                System.out.println("sifre = " + sifre);
+
+                Musteri musteri = new Musteri(fullname, tc, sifre);
+
+                if (databaseHelper.addMusteri(musteri) != -1){
+                    System.out.println("Eklendi");
+                } else {
+                    System.out.println("Hata");
+                }
+            }
+        });
     }
 }
