c48f821ba5a3c9b1e622c1be33cc35c08292622c
Turgay Ceylan <90829101+turgay2317@users.noreply.github.com>
2024-05-12 17:15:32 +0300

    exchange-ratios-made-dynamic

--- app/src/main/java/com/example/mobilebanking/CurrencyTableFragment.java
+++ app/src/main/java/com/example/mobilebanking/CurrencyTableFragment.java
@@ -1,57 +1,59 @@
 package com.example.mobilebanking;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CurrencyTableFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class CurrencyTableFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private Handler handler = new Handler(Looper.getMainLooper());
+    private ScheduledExecutorService executorService;
+    private TextView usdAlis;
+    private TextView usdSatis;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView euroAlis;
+    private TextView euroSatis;
+    private TextView bilgiYazisi;
+    private CountDownTimer countDownTimer;
+    private long timerMillis = 15000;
 
     public CurrencyTableFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CurrencyTableFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CurrencyTableFragment newInstance(String param1, String param2) {
-        CurrencyTableFragment fragment = new CurrencyTableFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            // JSON here
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
@@ -59,6 +61,104 @@ public class CurrencyTableFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_currency_table, container, false);
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleAtFixedRate(this::fetchDataAndUpdateUI, 0, 15, TimeUnit.SECONDS);
+
+
+        View view = inflater.inflate(R.layout.fragment_currency_table, container, false);
+        this.bilgiYazisi = view.findViewById(R.id.bilgiYazisi);
+        this.usdAlis = view.findViewById(R.id.dolarAlis);
+        this.usdSatis = view.findViewById(R.id.dolarSatis);
+        this.euroAlis = view.findViewById(R.id.euroAlis);
+        this.euroSatis = view.findViewById(R.id.euroSatis);
+
+        // Her saniye geri sayımı başlat
+        countDownTimer = new CountDownTimer(timerMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timerMillis = millisUntilFinished;
+                updateTimerText();
+            }
+
+            @Override
+            public void onFinish() {
+                timerMillis = 15000; // Geri sayım bittiğinde başlangıç süresini yeniden ayarla
+                startCountdownTimer(); // Geri sayımı başlat
+            }
+        };
+        startCountdownTimer();
+        fetchDataAndUpdateUI();
+        return view;
+    }
+
+    private void startCountdownTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+        countDownTimer.start();
+    }
+
+    private void updateTimerText() {
+        long secondsRemaining = timerMillis / 1000;
+        long minutes = secondsRemaining / 60;
+        long seconds = secondsRemaining % 60;
+        String timerText = String.format("%d", seconds);
+        bilgiYazisi.setText(timerText + " saniye sonra güncellenecek...");
+    }
+
+    private void fetchDataAndUpdateUI() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.genelpara.com/embed/doviz.json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String jsonData = response.body().string();
+            JSONObject jsonObject = new JSONObject(jsonData);
+
+            // JSON'dan gerekli verileri al
+            JSONObject usdObject = jsonObject.getJSONObject("USD");
+            String usdSatis = usdObject.getString("satis");
+            String usdAlis = usdObject.getString("alis");
+
+            JSONObject eurObject = jsonObject.getJSONObject("EUR");
+            String eurSatis = eurObject.getString("satis");
+            String eurAlis = eurObject.getString("alis");
+
+            // UI'yi güncelle
+            handler.post(() -> {
+                this.usdSatis.setText(usdSatis);
+                this.usdAlis.setText(usdAlis);
+                this.euroAlis.setText(eurAlis);
+                this.euroSatis.setText(eurSatis);
+                this.usdSatis.setBackgroundColor(Color.argb(30, 52, 196, 0));
+                this.usdAlis.setBackgroundColor(Color.argb(30, 52, 196, 0));
+                this.euroAlis.setBackgroundColor(Color.argb(30, 52, 196, 0));
+                this.euroSatis.setBackgroundColor(Color.argb(30, 52, 196, 0));
+                new Handler().postDelayed(() -> {
+                    this.usdSatis.setBackgroundColor(Color.TRANSPARENT);
+                    this.usdAlis.setBackgroundColor(Color.TRANSPARENT);
+                    this.euroAlis.setBackgroundColor(Color.TRANSPARENT);
+                    this.euroSatis.setBackgroundColor(Color.TRANSPARENT);
+                }, 2000);
+            });
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Uygulama kapanırken zamanlayıcıları durdur
+        if (executorService != null && !executorService.isShutdown()) {
+            executorService.shutdown();
+        }
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
     }
 }
